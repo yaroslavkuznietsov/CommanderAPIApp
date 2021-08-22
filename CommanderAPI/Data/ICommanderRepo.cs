9ed774b05300e7b7f0cf4920b8c6891b6cacc039
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-08-22 18:54:42 +0300

    First push. Created App, Created Command Class + Interface

---
+++ CommanderAPI/Data/ICommanderRepo.cs
@@ -0,0 +1,14 @@
+﻿using CommanderAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderAPI.Data
+{
+    public interface ICommanderRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int Id);
+    }
+}
---
+++ CommanderAPI/Data/MockCommanderRepo.cs
@@ -0,0 +1,21 @@
+﻿using CommanderAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderAPI.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Command GetCommandById(int Id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+        }
+    }
+}
