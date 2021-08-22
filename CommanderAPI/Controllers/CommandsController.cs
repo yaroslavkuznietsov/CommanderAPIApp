e99426e60b2846d05a9fd84a35a74a3d42fdc638
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-08-22 19:26:36 +0300

    Added CommandsController with two Get requests all + by id

---
+++ CommanderAPI/Controllers/CommandsController.cs
@@ -0,0 +1,37 @@
+﻿using CommanderAPI.Data;
+using CommanderAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderAPI.Controllers
+{
+    // api/commands
+    // [Route("api/[controller]")]
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repo = new MockCommanderRepo();
+
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repo.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        // GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repo.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+    }
+}
--- CommanderAPI/Data/MockCommanderRepo.cs
+++ CommanderAPI/Data/MockCommanderRepo.cs
@@ -10,7 +10,14 @@ namespace CommanderAPI.Data
     {
         public IEnumerable<Command> GetAppCommands()
         {
-            throw new NotImplementedException();
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" },
+                new Command { Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "Knife & Choping board" },
+                new Command { Id = 2, HowTo = "Make a cup of tea", Line = "Place teabag in cup", Platform = "Kettle & Cup" }
+            };
+
+            return commands;
         }
 
         public Command GetCommandById(int Id)
