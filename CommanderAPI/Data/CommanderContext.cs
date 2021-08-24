6f7fcb8c895b1d0f03d3832647bb4b743911d769
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-08-25 00:20:04 +0300

    CommanderContext.cs

---
+++ CommanderAPI/Data/CommanderContext.cs
@@ -0,0 +1,20 @@
+﻿using CommanderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderAPI.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+
+    }
+}
