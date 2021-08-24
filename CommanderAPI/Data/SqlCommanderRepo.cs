bedb56a52351c952ca8e8f325e4939d1e8a8d172
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-08-25 01:20:47 +0300

    SqlCommanderRepo.cs + swap it with MockCommanderRepo in Sturtup.cs

---
+++ CommanderAPI/Data/SqlCommanderRepo.cs
@@ -0,0 +1,28 @@
+﻿using CommanderAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderAPI.Data
+{
+    public class SqlCommanderRepo : ICommanderRepo
+    {
+        private readonly CommanderContext _context;
+
+        public SqlCommanderRepo(CommanderContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Command> GetAppCommands()
+        {
+            return _context.Commands.ToList();
+        }
+
+        public Command GetCommandById(int Id)
+        {
+            return _context.Commands.Where(c => c.Id == Id).FirstOrDefault();
+        }
+    }
+}
--- CommanderAPI/Startup.cs
+++ CommanderAPI/Startup.cs
@@ -31,7 +31,7 @@ namespace CommanderAPI
 
             services.AddControllers();
 
-            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();    // MockCommanderRepo <-> SqlCommanderRepo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
